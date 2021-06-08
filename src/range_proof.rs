260dda3403d89838db2ef9f4980058bc1024d785
Sam Kim <skim13@cs.stanford.edu>
2021-06-08 15:31:28 -0400

    Remove randomized components

---
+++ src/range_proof.rs
@@ -0,0 +1,246 @@
+#![allow(non_snake_case)]
+
+use core::iter;
+
+use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
+use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::traits::{IsIdentity, VartimeMultiscalarMul};
+use merlin::Transcript;
+
+use crate::errors::ProofError;
+use crate::generators::{BulletproofGens, PedersenGens};
+use crate::inner_product_proof::InnerProductProof;
+use crate::transcript::TranscriptProtocol;
+use crate::util;
+
+/// The `RangeProof` struct represents a proof that one or more values
+/// are in a range.
+///
+/// The `RangeProof` struct contains functions for creating and
+/// verifying aggregated range proofs.  The single-value case is
+/// implemented as a special case of aggregated range proofs.
+///
+/// The bitsize of the range, as well as the list of commitments to
+/// the values, are not included in the proof, and must be known to
+/// the verifier.
+///
+/// This implementation requires that both the bitsize `n` and the
+/// aggregation size `m` be powers of two, so that `n = 8, 16, 32, 64`
+/// and `m = 1, 2, 4, 8, 16, ...`.  Note that the aggregation size is
+/// not given as an explicit parameter, but is determined by the
+/// number of values or commitments passed to the prover or verifier.
+///
+/// # Note
+///
+/// For proving, these functions run the multiparty aggregation
+/// protocol locally.  That API is exposed in the [`aggregation`](::range_proof_mpc)
+/// module and can be used to perform online aggregation between
+/// parties without revealing secret values to each other.
+#[derive(Clone, Debug)]
+pub struct RangeProof {
+    /// Commitment to the bits of the value
+    A: CompressedRistretto,
+    /// Commitment to the blinding factors
+    S: CompressedRistretto,
+    /// Commitment to the \\(t_1\\) coefficient of \\( t(x) \\)
+    T_1: CompressedRistretto,
+    /// Commitment to the \\(t_2\\) coefficient of \\( t(x) \\)
+    T_2: CompressedRistretto,
+    /// Evaluation of the polynomial \\(t(x)\\) at the challenge point \\(x\\)
+    t_x: Scalar,
+    /// Blinding factor for the synthetic commitment to \\(t(x)\\)
+    t_x_blinding: Scalar,
+    /// Blinding factor for the synthetic commitment to the inner-product arguments
+    e_blinding: Scalar,
+    /// Proof data for the inner-product argument.
+    ipp_proof: InnerProductProof,
+}
+
+impl RangeProof {
+    /// Verifies a rangeproof for a given value commitment \\(V\\).
+    pub fn verify_single(
+        &self,
+        bp_gens: &BulletproofGens,
+        pc_gens: &PedersenGens,
+        transcript: &mut Transcript,
+        V: &CompressedRistretto,
+        n: usize,
+    ) -> Result<(), ProofError> {
+        let m = 1;
+        let value_commitments = &[*V];
+
+        // First, replay the "interactive" protocol using the proof
+        // data to recompute all challenges.
+        if !(n == 8 || n == 16 || n == 32 || n == 64) {
+            return Err(ProofError::InvalidBitsize);
+        }
+        if bp_gens.gens_capacity < n {
+            return Err(ProofError::InvalidGeneratorsLength);
+        }
+        if bp_gens.party_capacity < m {
+            return Err(ProofError::InvalidGeneratorsLength);
+        }
+
+        transcript.rangeproof_domain_sep(n as u64, m as u64);
+
+        for V in value_commitments.iter() {
+            // Allow the commitments to be zero (0 value, 0 blinding)
+            // See https://github.com/dalek-cryptography/bulletproofs/pull/248#discussion_r255167177
+            transcript.append_point(b"V", V);
+        }
+
+        transcript.validate_and_append_point(b"A", &self.A)?;
+        transcript.validate_and_append_point(b"S", &self.S)?;
+
+        let y = transcript.challenge_scalar(b"y");
+        let z = transcript.challenge_scalar(b"z");
+        let zz = z * z;
+        let minus_z = -z;
+
+        transcript.validate_and_append_point(b"T_1", &self.T_1)?;
+        transcript.validate_and_append_point(b"T_2", &self.T_2)?;
+
+        let x = transcript.challenge_scalar(b"x");
+
+        transcript.append_scalar(b"t_x", &self.t_x);
+        transcript.append_scalar(b"t_x_blinding", &self.t_x_blinding);
+        transcript.append_scalar(b"e_blinding", &self.e_blinding);
+
+        let w = transcript.challenge_scalar(b"w");
+
+        // Challenge value for batching statements to be verified
+        // let c = Scalar::random(rng);
+        let c = transcript.challenge_scalar(b"c");
+
+        let (x_sq, x_inv_sq, s) = self.ipp_proof.verification_scalars(n * m, transcript)?;
+        let s_inv = s.iter().rev();
+
+        let a = self.ipp_proof.a;
+        let b = self.ipp_proof.b;
+
+        // Construct concat_z_and_2, an iterator of the values of
+        // z^0 * \vec(2)^n || z^1 * \vec(2)^n || ... || z^(m-1) * \vec(2)^n
+        let powers_of_2: Vec<Scalar> = util::exp_iter(Scalar::from(2u64)).take(n).collect();
+        let concat_z_and_2: Vec<Scalar> = util::exp_iter(z)
+            .take(m)
+            .flat_map(|exp_z| powers_of_2.iter().map(move |exp_2| exp_2 * exp_z))
+            .collect();
+
+        let g = s.iter().map(|s_i| minus_z - a * s_i);
+        let h = s_inv
+            .zip(util::exp_iter(y.invert()))
+            .zip(concat_z_and_2.iter())
+            .map(|((s_i_inv, exp_y_inv), z_and_2)| z + exp_y_inv * (zz * z_and_2 - b * s_i_inv));
+
+        let value_commitment_scalars = util::exp_iter(z).take(m).map(|z_exp| c * zz * z_exp);
+        let basepoint_scalar = w * (self.t_x - a * b) + c * (delta(n, m, &y, &z) - self.t_x);
+
+        let mega_check = RistrettoPoint::optional_multiscalar_mul(
+            iter::once(Scalar::one())
+                .chain(iter::once(x))
+                .chain(iter::once(c * x))
+                .chain(iter::once(c * x * x))
+                .chain(x_sq.iter().cloned())
+                .chain(x_inv_sq.iter().cloned())
+                .chain(iter::once(-self.e_blinding - c * self.t_x_blinding))
+                .chain(iter::once(basepoint_scalar))
+                .chain(g)
+                .chain(h)
+                .chain(value_commitment_scalars),
+            iter::once(self.A.decompress())
+                .chain(iter::once(self.S.decompress()))
+                .chain(iter::once(self.T_1.decompress()))
+                .chain(iter::once(self.T_2.decompress()))
+                .chain(self.ipp_proof.L_vec.iter().map(|L| L.decompress()))
+                .chain(self.ipp_proof.R_vec.iter().map(|R| R.decompress()))
+                .chain(iter::once(Some(pc_gens.B_blinding)))
+                .chain(iter::once(Some(pc_gens.B)))
+                .chain(bp_gens.G(n, m).map(|&x| Some(x)))
+                .chain(bp_gens.H(n, m).map(|&x| Some(x)))
+                .chain(value_commitments.iter().map(|V| V.decompress())),
+        )
+        .ok_or_else(|| ProofError::VerificationError)?;
+
+        if mega_check.is_identity() {
+            Ok(())
+        } else {
+            Err(ProofError::VerificationError)
+        }
+    }
+
+    /// Serializes the proof into a byte array of \\(2 \lg n + 9\\)
+    /// 32-byte elements, where \\(n\\) is the number of secret bits.
+    ///
+    /// # Layout
+    ///
+    /// The layout of the range proof encoding is:
+    ///
+    /// * four compressed Ristretto points \\(A,S,T_1,T_2\\),
+    /// * three scalars \\(t_x, \tilde{t}_x, \tilde{e}\\),
+    /// * \\(n\\) pairs of compressed Ristretto points \\(L_0,R_0\dots,L_{n-1},R_{n-1}\\),
+    /// * two scalars \\(a, b\\).
+    pub fn to_bytes(&self) -> Vec<u8> {
+        // 7 elements: points A, S, T1, T2, scalars tx, tx_bl, e_bl.
+        let mut buf = Vec::with_capacity(7 * 32 + self.ipp_proof.serialized_size());
+        buf.extend_from_slice(self.A.as_bytes());
+        buf.extend_from_slice(self.S.as_bytes());
+        buf.extend_from_slice(self.T_1.as_bytes());
+        buf.extend_from_slice(self.T_2.as_bytes());
+        buf.extend_from_slice(self.t_x.as_bytes());
+        buf.extend_from_slice(self.t_x_blinding.as_bytes());
+        buf.extend_from_slice(self.e_blinding.as_bytes());
+        buf.extend(self.ipp_proof.to_bytes_iter());
+        buf
+    }
+
+    /// Deserializes the proof from a byte slice.
+    ///
+    /// Returns an error if the byte slice cannot be parsed into a `RangeProof`.
+    pub fn from_bytes(slice: &[u8]) -> Result<RangeProof, ProofError> {
+        if slice.len() % 32 != 0 {
+            return Err(ProofError::FormatError);
+        }
+        if slice.len() < 7 * 32 {
+            return Err(ProofError::FormatError);
+        }
+
+        use crate::util::read32;
+
+        let A = CompressedRistretto(read32(&slice[0 * 32..]));
+        let S = CompressedRistretto(read32(&slice[1 * 32..]));
+        let T_1 = CompressedRistretto(read32(&slice[2 * 32..]));
+        let T_2 = CompressedRistretto(read32(&slice[3 * 32..]));
+
+        let t_x = Scalar::from_canonical_bytes(read32(&slice[4 * 32..]))
+            .ok_or(ProofError::FormatError)?;
+        let t_x_blinding = Scalar::from_canonical_bytes(read32(&slice[5 * 32..]))
+            .ok_or(ProofError::FormatError)?;
+        let e_blinding = Scalar::from_canonical_bytes(read32(&slice[6 * 32..]))
+            .ok_or(ProofError::FormatError)?;
+
+        let ipp_proof = InnerProductProof::from_bytes(&slice[7 * 32..])?;
+
+        Ok(RangeProof {
+            A,
+            S,
+            T_1,
+            T_2,
+            t_x,
+            t_x_blinding,
+            e_blinding,
+            ipp_proof,
+        })
+    }
+}
+
+/// Compute
+/// \\[
+/// \delta(y,z) = (z - z^{2}) \langle \mathbf{1}, {\mathbf{y}}^{n \cdot m} \rangle - \sum_{j=0}^{m-1} z^{j+3} \cdot \langle \mathbf{1}, {\mathbf{2}}^{n \cdot m} \rangle
+/// \\]
+fn delta(n: usize, m: usize, y: &Scalar, z: &Scalar) -> Scalar {
+    let sum_y = util::sum_of_powers(y, n * m);
+    let sum_2 = util::sum_of_powers(&Scalar::from(2u64), n);
+    let sum_z = util::sum_of_powers(z, m);
+
+    (z - z * z) * sum_y - z * z * z * sum_2 * sum_z
+}
